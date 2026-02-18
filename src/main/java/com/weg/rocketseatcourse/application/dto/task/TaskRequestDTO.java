b18169cf3bb1b83097d2e040196b59f48a3a34e9
jonathan_uber <jonathanuber121207@gmail.com>
2026-02-18 15:53:12 -0300

    fix: add validation in DTO classes, remove from entity

--- src/main/java/com/weg/rocketseatcourse/application/dto/task/TaskRequestDTO.java
+++ src/main/java/com/weg/rocketseatcourse/application/dto/task/TaskRequestDTO.java
@@ -1,13 +1,23 @@
 package com.weg.rocketseatcourse.application.dto.task;
 
+import jakarta.persistence.Column;
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.NotNull;
+
 import java.time.LocalDateTime;
 import java.util.UUID;
 
-public record TaskRequestDTO (String title,
-                              String description,
-                              LocalDateTime startAt,
-                              LocalDateTime endAt,
-                              String priority,
-                              UUID user_id
-                              ){
+public record TaskRequestDTO (
+        @NotEmpty(message = "Title can't be empty")
+        @NotNull(message = "Title can't be null")
+        String title,
+
+        @NotEmpty(message = "Description can't be empty")
+        @NotNull(message = "Description can't be null")
+        String description,
+        LocalDateTime startAt,
+        LocalDateTime endAt,
+        String priority,
+        UUID user_id
+){
 }
