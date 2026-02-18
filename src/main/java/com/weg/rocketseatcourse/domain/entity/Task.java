ee5d6aa2378a51b46dc63930eabbde35270fcacb
jonathan_uber <jonathanuber121207@gmail.com>
2026-02-18 00:43:35 -0300

    feat: implementa entidade Task

--- src/main/java/com/weg/rocketseatcourse/domain/entity/Task.java
+++ src/main/java/com/weg/rocketseatcourse/domain/entity/Task.java
@@ -1,4 +1,45 @@
 package com.weg.rocketseatcourse.domain.entity;
 
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.hibernate.annotations.CreationTimestamp;
+
+import java.time.LocalDateTime;
+import java.util.UUID;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity(name = "Task")
 public class Task {
+
+    @Id
+    @GeneratedValue(generator = "UUID")
+    private UUID id;
+
+    @NotEmpty
+    @NotNull
+    @Column(length = 50)
+    private String title;
+
+    @NotEmpty
+    @NotNull
+    @Column(length = 255)
+    private String description;
+    private String startAt;
+    private String endAt;
+    private String priority;
+    private UUID user_id;
+
+    @CreationTimestamp
+    private LocalDateTime createdAt;
+
 }
