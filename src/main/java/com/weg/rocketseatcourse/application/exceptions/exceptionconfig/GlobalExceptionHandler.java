0c07baa9a1f9db8c3725866690af3058e8517c7b
jonathan_uber <jonathanuber121207@gmail.com>
2026-02-18 15:44:48 -0300

    feat: implement global exception handler

--- src/main/java/com/weg/rocketseatcourse/application/exceptions/exceptionconfig/GlobalExceptionHandler.java
+++ src/main/java/com/weg/rocketseatcourse/application/exceptions/exceptionconfig/GlobalExceptionHandler.java
@@ -1,12 +1,67 @@
 package com.weg.rocketseatcourse.application.exceptions.exceptionconfig;
 
+import com.weg.rocketseatcourse.application.exceptions.*;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
+import java.time.LocalDateTime;
+
 @RestControllerAdvice
-public class GlobalExceptionHandler extends RuntimeException {
+public class GlobalExceptionHandler{
 
-//    @ExceptionHandler(RuntimeException.class)
-//    public
+    private ResponseEntity<ErrorResponse> buildError(HttpStatus status, String message) {
+        ErrorResponse error = new ErrorResponse(
+                LocalDateTime.now(),
+                status.value(),
+                message
+        );
+        return ResponseEntity.status(status).body(error);
+    }
 
+    @ExceptionHandler(Exception.class)
+    public ResponseEntity<ErrorResponse> handleGeneric(Exception ex){
+        return buildError(HttpStatus.INTERNAL_SERVER_ERROR, "Internal server error!");
+    }
+
+    @ExceptionHandler(EmailAlreadyExistsException.class)
+    public ResponseEntity<ErrorResponse> handleEmailExists(EmailAlreadyExistsException ex){
+        return buildError(HttpStatus.CONFLICT, ex.getMessage());
+    }
+
+    @ExceptionHandler(InvalidEmailException.class)
+    public ResponseEntity<ErrorResponse> handleInvalidEmail(InvalidEmailException ex){
+        return buildError(HttpStatus.BAD_REQUEST, ex.getMessage());
+    }
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<ErrorResponse> handleValidationErrors(MethodArgumentNotValidException ex) {
+        String message = ex.getBindingResult()
+                .getFieldError()
+                .getDefaultMessage();
+
+        return buildError(HttpStatus.BAD_REQUEST, message);
+    }
+
+    @ExceptionHandler(TaskCantBeNullException.class)
+    public ResponseEntity<ErrorResponse> handleTaskNull(TaskCantBeNullException ex){
+        return buildError(HttpStatus.BAD_REQUEST, ex.getMessage());
+    }
+
+    @ExceptionHandler(UserCantBeNullException.class)
+    public ResponseEntity<ErrorResponse> handleUserNull(UserCantBeNullException ex){
+        return buildError(HttpStatus.BAD_REQUEST, ex.getMessage());
+    }
+
+    @ExceptionHandler(TaskNotFoundException.class)
+    public ResponseEntity<ErrorResponse> handleTaskNotFound(TaskNotFoundException ex){
+        return buildError(HttpStatus.NOT_FOUND, ex.getMessage());
+    }
+
+    @ExceptionHandler(UserNotFoundException.class)
+    public ResponseEntity<ErrorResponse> handleUserNotFound(UserNotFoundException ex){
+        return buildError(HttpStatus.NOT_FOUND, ex.getMessage());
+    }
 }
