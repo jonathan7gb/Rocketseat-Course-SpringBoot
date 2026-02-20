e99a0c3b96cc19dc6b56eba36d23d88802a1b174
jonathan_uber <jonathanuber121207@gmail.com>
2026-02-20 16:52:54 -0300

    feat: add more validations on task request dto

--- src/main/java/com/weg/rocketseatcourse/application/dto/task/TaskRequestDTO.java
+++ src/main/java/com/weg/rocketseatcourse/application/dto/task/TaskRequestDTO.java
@@ -3,15 +3,18 @@ package com.weg.rocketseatcourse.application.dto.task;
 import com.weg.rocketseatcourse.domain.enums.TaskPriority;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
 
 
 import java.util.UUID;
 
 public record TaskRequestDTO (
         @NotBlank(message = "Title can't be blank")
+        @Size(min = 5, max = 50, message = "Title must be between 5 and 50 characters")
         String title,
 
         @NotBlank(message = "Description can't be blank")
+        @Size(min = 5, max = 255, message = "Title must be between 5 and 255 characters")
         String description,
 
         @NotBlank(message = "Priority can't be blank")
