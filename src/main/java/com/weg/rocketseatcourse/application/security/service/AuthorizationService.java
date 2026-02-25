9660703c001cc78fd08e419a1f4ae6942510f117
jonathan_uber <jonathanuber121207@gmail.com>
2026-02-24 21:23:06 -0300

    feat: implement security configuration file

--- src/main/java/com/weg/rocketseatcourse/application/security/service/AuthorizationService.java
+++ src/main/java/com/weg/rocketseatcourse/application/security/service/AuthorizationService.java
@@ -1,6 +1,7 @@
 package com.weg.rocketseatcourse.application.security.service;
 
 import com.weg.rocketseatcourse.application.exceptions.UserNotFoundException;
+import com.weg.rocketseatcourse.domain.repository.UserRepository;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.stereotype.Service;
@@ -8,8 +9,14 @@ import org.springframework.stereotype.Service;
 @Service
 public class AuthorizationService implements UserDetailsService {
 
+    private final UserRepository userRepository;
+
+    public AuthorizationService(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
     @Override
-    public UserDetails loadUserByUsername(String username) throws UserNotFoundException{
-        return null;
+    public UserDetails loadUserByUsername(String email) throws UserNotFoundException{
+        return userRepository.findByEmail(email);
     }
 }
